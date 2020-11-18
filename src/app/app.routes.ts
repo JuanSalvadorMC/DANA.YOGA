5bbcfc4ca14136acba52d457876c0dbaebc55b4e
Salvador M.Cervantes <47789943+JuanSalvadorMC@users.noreply.github.com>
2020-11-18 02:06:21 -0600

    m

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { from } from "rxjs";
 import {Routes} from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {LessonsComponent} from './views/lessons/lessons.component';
+import { FormComponent } from './views/lessons/form/form.component';
 /* import { MeditacionComponent } from './views/meditacion/meditacion.component';
  */
 
@@ -9,7 +10,9 @@ export const ROUTES: Routes = [
     {path:'home', component: HomeComponent},
   /*   {path:'meditacion', component: MeditacionComponent}, */
     {path:'lessons', component: LessonsComponent},
+    {path:'lessons-form', component: FormComponent},
     {path:'', pathMatch:'full', redirectTo:'home'},
     {path:'**', pathMatch:'full', redirectTo:'home'},
+    
 
 ];
